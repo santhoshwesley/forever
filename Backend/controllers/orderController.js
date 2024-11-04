c686dd3f65f94d8ff4deb801f952f704e67e24c8
Vadlapudisravani <sravanivadlapudi1@gmail.com>
2024-11-04 21:01:16 +0530

    added payment verify page

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,5 +1,13 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Stripe from 'stripe'
+
+// global variables
+const currency = 'usd'
+const deliveryCharge = 10
+
+// gateway initialize
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // Placing Orders Using COD Method
 const placeOrder = async (req, res) => {
@@ -29,13 +37,98 @@ const placeOrder = async (req, res) => {
 };
 
 //  Placing Orders Using Stripe Method
-const placeOrderStripe = async (req, res) => {};
+const placeOrderStripe = async (req, res) => {
+  try {
+    
+    const { userId, items, amount, address } = req.body;
+    const {origin} = req.headers;
+
+    const orderData = {
+      userId,
+      items,
+      address,
+      amount,
+      paymentMethod: "Stripe",
+      payment: false,
+      date: Date.now(),
+    };
+
+    const newOrder = new orderModel(orderData);
+    await newOrder.save()
+
+    const line_items = items.map((item)=> ({
+      price_data : {
+        currency: currency,
+        product_data: {
+          name:item.name,
+        },
+        unit_amount : item.price * 100,
+      },
+      quantity : item.quantity,
+    }));
+
+    line_items.push({
+      price_data : {
+        currency: currency,
+        product_data: {
+          name:"Delivery Charges",
+        },
+        unit_amount : deliveryCharge * 100,
+      },
+      quantity : 1,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+       success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+       cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+       line_items,
+       mode:"payment",
+    });
+
+    res.json({ success: true, session_url: session.url });
+
+
+       
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// verify stripe
+const verifyStripe = async (req,res) => {
+  const { orderId, success, userId } = req.body
+
+  try {
+    if (success === "true") {
+      await orderModel.findByIdAndUpdate(orderId, {payment:true});
+      await userModel.findByIdAndUpdate(userId, {cartData: {}})
+      res.json({success:true});
+    } else {
+      await orderModel.findByIdAndDelete(orderId)
+      res.json({success:false})
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+}
 
 //  Placing Orders Using Razorpay Method
 const placeOrderRazorpay = async (req, res) => {};
 
 // All Orders data for Admin Panel
-const allOrders = async (req, res) => {};
+const allOrders = async (req, res) => {
+   try {
+
+    const orders = await orderModel.find({})
+    res.json({success:true, orders})
+    
+   } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+   }
+};
 
 // User Order Data for Frontend
 const userOrders = async (req, res) => {
@@ -43,9 +136,9 @@ const userOrders = async (req, res) => {
     const {userId} = req.body;
     const orders = await orderModel.find({ userId })
 
-    if (!orders) {
-      return res.json({ success: false, message: "No orders found" });
-    }
+    // if (!orders) {
+    //   return res.json({ success: false, message: "No orders found" });
+    // }
 
     res.json({ success: true, orders });
   } catch (error) {
@@ -55,9 +148,19 @@ const userOrders = async (req, res) => {
 };
 
 // Update Order Status from Admin Panel
-const updateStatus = async (req, res) => {};
+const updateStatus = async (req, res) => {
+  try {
+    const { orderId, status } = req.body
+    await orderModel.findByIdAndUpdate(orderId, { status })
+    res.json({success:true, message:'Status Updated'})
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export {
+  verifyStripe,
   placeOrder,
   placeOrderStripe,
   placeOrderRazorpay,
--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -37,64 +37,7 @@ const PlaceOrder = () => {
     setDeliveryInfo((prev) => ({ ...prev, [name]: value }));
   };
 
-  // const onSubmitHandler = async (event) => {
-  //   event.preventDefault();
-  //   try {
-  //     let orderItems = [];
-
-  //     for (const items in cartItems) {
-  //       for (const item in cartItems[items]) {
-  //         if (cartItems[items][item] > 0) {
-  //           const itemInfo = structuredClone(
-  //             products.find((product) => product._id === items)
-  //           );
-  //           if (itemInfo) {
-  //             itemInfo.size = item;
-  //             itemInfo.quantity = cartItems[items][item];
-  //             orderItems.push(itemInfo);
-  //           }
-  //         }
-  //       }
-  //     }
-  
-  
-      
-  
-
-  //     let orderData = {
-  //       addess: formData,
-  //       items: orderItems,
-  //       amount: getCartAmount() + delivery_fee
-  //     }
-
-  //     switch (method) {
-
-  //       // API calls for COD orders
-  //       case 'cod' :
-  //           const response = await axios.post(backendUrl + '/api/order/place',orderData,{headers:{token}})
-  //           if (response.data.success) {
-  //             setCartItems({})
-  //             navigate('/orders')
-  //           } else {
-  //             toast.error(response.data.message)
-  //           }
-  //       break;
-
-  //       default:
-  //         break;
-  //     }
-
-  //     // Place order with the assembled data
-  //     await placeOrder({ ...deliveryInfo, paymentMethod: method, orderItems });
-
-  //     // Optionally clear cart or navigate upon successful order
-  //     setCartItems({});
-  //     navigate("/confirmation");
-  //   } catch (error) {
-  //     console.error("Error placing order:", error);
-  //   }
-  // };
-
+ 
   const onSubmitHandler = async (event) => {
     event.preventDefault(); // Prevent the default form submission
     try {
@@ -136,6 +79,19 @@ const PlaceOrder = () => {
           } else {
             toast.error(response.data.message);
           }
+          break;
+
+          case 'stripe' : 
+           const responseStripe = await axios.post(backendUrl + '/api/order/stripe',orderData,{headers:{token}})
+           console.log(responseStripe.data);
+           
+             if (responseStripe.data.success) {
+              const {session_url} = responseStripe.data
+              window.location.replace(session_url)
+             } else {
+              toast.error(responseStripe.data.message)
+             }
+
           break;
   
         // Handle other payment methods here if necessary
